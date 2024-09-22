73c89ec379b2a8b993a1b968960b6fd9ec95a2af
kanaru-ssk <kanaru.ssk@gmail.com>
2024-09-22 03:03:36 +0000

    update

--- src/app/components/users/training-page/detailed-course-page/detailed-course-page.component.ts
+++ src/app/components/users/training-page/detailed-course-page/detailed-course-page.component.ts
@@ -8,4 +8,11 @@ import { RouterLink } from "@angular/router";
   templateUrl: "./detailed-course-page.component.html",
   styleUrl: "./detailed-course-page.component.scss",
 })
-export class DetailedCoursePageComponent {}
+export class DetailedCoursePageComponent {
+  latitude: number = -36.85248871196826;
+  longitude: number = 174.77003145479162;
+
+  get googleMapsUrl(): string {
+    return `https://www.google.com/maps?q=${this.latitude},${this.longitude}`;
+  }
+}
--- src/app/components/users/training-page/training-list-page/training-list-page.component.ts
+++ src/app/components/users/training-page/training-list-page/training-list-page.component.ts
@@ -8,4 +8,11 @@ import { RouterLink } from "@angular/router";
   templateUrl: "./training-list-page.component.html",
   styleUrl: "./training-list-page.component.scss",
 })
-export class TrainingListPageComponent {}
+export class TrainingListPageComponent {
+  latitude: number = -36.85248871196826;
+  longitude: number = 174.77003145479162;
+
+  get googleMapsUrl(): string {
+    return `https://www.google.com/maps?q=${this.latitude},${this.longitude}`;
+  }
+}
