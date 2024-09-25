413edb7eab1612a9bc5e98740c6a583c2279ce68
ksho877 <kay.shoji.july@gmail.com>
2024-09-25 18:49:13 +1200

    update

--- src/app/shared/routes/routes.ts
+++ src/app/shared/routes/routes.ts
@@ -6,13 +6,6 @@ export const content: Routes = [
     data: {
       breadcrumb: "Users",
     },
-    loadChildren: () => import("../../components/users/users.routes"),
-  },
-  {
-    path: "admin",
-    data: {
-      breadcrumb: "Admin",
-    },
-    loadChildren: () => import("../../components/admin/admin.routes"),
+    loadChildren: () => import("../../components/pages/users.routes"),
   },
 ];
